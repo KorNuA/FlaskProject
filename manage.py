9900390a7c4b257a0062fca4a3267d2a9059d046
KorNuA <amm58707698388828@163.com>
2021-05-28 12:52:47 +0800

    项目基本配置-MySQL

--- manage.py
+++ manage.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+# Python2&Python3数据库相互转换
+pymysql.install_as_MySQLdb()
+
+
+class Config:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root@127.0.0.1:3306/information23"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 
 @app.route("/index")
